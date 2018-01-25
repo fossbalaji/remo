4fdb1f8b92728378cc5819719e67819e65543aab
fossbalaji <fossbalaji@gmail.com>
2018-01-25 20:56:53 +0530

    [fix bug 1245643] user object replaced by commenter

--- remo/events/models.py
+++ remo/events/models.py
@@ -281,8 +281,8 @@ def email_event_owner_on_add_comment(sender, instance, **kwargs):
     event = instance.event
     owner = instance.event.owner
     event_url = reverse('events_view_event', kwargs={'slug': event.slug})
-    ctx_data = {'event': event, 'owner': owner, 'user': instance.user,
-                'comment': instance.comment, 'event_url': event_url, 'commenter': instance.user}
+    ctx_data = {'event': event, 'owner': owner, 'commenter': instance.user,
+                'comment': instance.comment, 'event_url': event_url}
     if owner.userprofile.receive_email_on_add_event_comment:
         subject = subject % (instance.user.get_full_name(), instance.event.name)
         send_remo_mail.delay(subject=subject, recipients_list=[owner.id],
